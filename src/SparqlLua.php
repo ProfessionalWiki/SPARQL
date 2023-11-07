cfda05cf850358c9e6d41539cab32e6b1a2dae33
waximabbax <waximabbax@gmail.com>
2023-11-07 18:05:37 +0500

    integrated SPARQL Query; addresses SPARQL-issue#1

--- src/SparqlLua.php
+++ src/SparqlLua.php
@@ -4,27 +4,31 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\SPARQL;
 
+use InvalidArgumentException;
+use MediaWiki\MediaWikiServices;
 use Scribunto_LuaLibraryBase;
 
 class SparqlLua extends Scribunto_LuaLibraryBase {
 
-	/**
-	 * @return mixed[]
-	 */
 	public function register(): array {
 		return $this->getEngine()->registerInterface(
 			__DIR__ . '/Sparql.lua',
 			[
-				'runQuery' => fn( string $subjectId ): array => [ $this->runQuery( $subjectId ) ],
+				'runQuery' => fn ( string $subjectId ): array => [ $this->runSparqlQuery( $subjectId ) ],
 			]
 		);
 	}
 
-	/**
-	 * @return mixed[]
-	 */
-	private function runQuery( string $sparql ): array {
-		return [ $sparql ]; // TODO
+	private function runSparqlQuery( string $sparql ): string|null {
+		$sparqlEndpoint = MediaWikiServices::getInstance()->getMainConfig()->get( 'SPARQLEndpoint' );
+
+		if ( !is_string( $sparqlEndpoint ) ) {
+			throw new InvalidArgumentException( 'SPARQLEndpoint must be configured properly' );
+		}
+
+		$requestFactory = MediaWikiServices::getInstance()->getHttpRequestFactory();
+		$queryRunner = new SparqlLuaQueryRunner( $requestFactory, $sparqlEndpoint );
+		return $queryRunner->runQuery( $sparql );
 	}
 
 }
---
+++ tests/SparqlLuaQueryRunnerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\SPARQL\Tests;
+
+use MediaWiki\Http\HttpRequestFactory;
+use PHPUnit\Framework\TestCase;
+use ProfessionalWiki\SPARQL\SparqlLuaQueryRunner;
+
+/**
+ * @covers SparqlLuaQueryRunner::runQuery
+ */
+class SparqlLuaQueryRunnerTest extends TestCase {
+
+	public function testRunQuery() {
+		$SPARQLEndpoint = 'https://www.example.com';
+		$httpRequestFactory = $this->createMock( HttpRequestFactory::class );
+		$sparqlLua = new SparqlLuaQueryRunner( $httpRequestFactory, $SPARQLEndpoint );
+
+		$sparql = 'SELECT * WHERE { ?s ?p ?o }';
+		$expectedResponse = 'mocking response';
+
+		$httpRequestFactory
+			->expects( $this->once() )
+			->method( 'request' )
+			->with(
+				'POST',
+				$SPARQLEndpoint,
+				[
+					'postData' => [ 'query' => $sparql, 'format' => 'json' ]
+				]
+			)
+			->willReturn( $expectedResponse );
+
+		$response = $sparqlLua->runQuery( $sparql );
+		$this->assertSame( $expectedResponse, $response );
+	}
+
+}
