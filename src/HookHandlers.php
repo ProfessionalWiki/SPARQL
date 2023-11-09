a23f0663ef9b5d0b14ecd519a8aca92b16748a95
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-11-09 20:15:02 +0100

    Allow real HTTP requests from the parser tests

--- src/HookHandlers.php
+++ src/HookHandlers.php
@@ -4,6 +4,12 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\SPARQL;
 
+use MediaWiki\Config\ServiceOptions;
+use MediaWiki\Http\HttpRequestFactory;
+use MediaWiki\Http\Telemetry;
+use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\MediaWikiServices;
+
 final class HookHandlers {
 
 	/**
@@ -11,6 +17,24 @@ final class HookHandlers {
 	 */
 	public static function onScribuntoExternalLibraries( string $engine, array &$extraLibraries ): void {
 		$extraLibraries['SPARQL'] = SparqlLua::class;
+	}
+
+	public static function onParserTestGlobals( array &$globals ): void {
+		MediaWikiServices::getInstance()->resetServiceForTesting( 'HttpRequestFactory' );
+		MediaWikiServices::getInstance()->redefineService(
+			'HttpRequestFactory',
+			// The below function is copied from ServiceWiring.php. Not sure how to access the original function.
+			static function ( MediaWikiServices $services ): HttpRequestFactory {
+				return new HttpRequestFactory(
+					new ServiceOptions(
+						HttpRequestFactory::CONSTRUCTOR_OPTIONS,
+						$services->getMainConfig()
+					),
+					LoggerFactory::getInstance( 'http' ),
+					Telemetry::getInstance()
+				);
+			}
+		);
 	}
 
 }
