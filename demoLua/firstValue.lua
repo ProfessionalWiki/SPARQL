1f2a96189e0f11ff83bc31811fa1fc47e01096e8
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-11-25 16:13:51 +0100

    Update firstValue.lua

--- demoLua/firstValue.lua
+++ demoLua/firstValue.lua
@@ -1,10 +1,15 @@
-local sparql = require('SPARQL')
+local sparql = require('SPARQL') -- Load the SPARQL binding
 
 local p = {}
 
 function p.showFirstValue(frame)
 	local sparqlQuery = frame.args[1]
-	local queryResults = sparql.runQuery(sparqlQuery)
+	local queryResults = sparql.runQuery(sparqlQuery) -- Use the runQuery method
+
+	-- nil is returned if Blazegraph did not return a valid response
+	if queryResults == nil then
+		return ''
+	end
 
 	-- Replace "work" with the first SELECT variable in your SPARQL query
 	return queryResults['results']['bindings'][1]['work']['value']
